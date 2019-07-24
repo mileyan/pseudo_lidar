789cec4e3d3a9d192fb4573ac66d0b3fee7a8aee
Mars <jdaims.lim@gmail.com>
2019-07-23 17:02:05 -0700

    ArgoGenLidar.py

---
+++ preprocessing/ArgoGenLidar.py
@@ -0,0 +1,56 @@
+import argparse
+import os
+
+import numpy as np
+import scipy.misc as ssc
+
+import kitti_util
+
+
+def project_disp_to_depth(calib, disp, max_high):
+    disp[disp < 0] = 0
+    baseline = 0.54
+    mask = disp > 0
+    depth = calib.f_u * baseline / (disp + 1. - mask)
+    rows, cols = depth.shape
+    c, r = np.meshgrid(np.arange(cols), np.arange(rows))
+    points = np.stack([c, r, depth])
+    points = points.reshape((3, -1))
+    points = points.T
+    points = points[mask.reshape(-1)]
+    cloud = calib.project_image_to_velo(points)
+    valid = (cloud[:, 0] >= 0) & (cloud[:, 2] < max_high)
+    return cloud[valid]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate Libar')
+    parser.add_argument('--calib_dir', type=str,
+                        default='~/Kitti/object/training/calib')
+    parser.add_argument('--disparity_dir', type=str,
+                        default='~/Kitti/object/training/predicted_disparity')
+    parser.add_argument('--save_dir', type=str,
+                        default='~/Kitti/object/training/predicted_velodyne')
+    parser.add_argument('--max_high', type=int, default=1)
+    args = parser.parse_args()
+
+    assert os.path.isdir(args.disparity_dir)
+    assert os.path.isdir(args.calib_dir)
+
+    if not os.path.isdir(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    disps = [x for x in os.listdir(args.disparity_dir) if x[-3:] == 'png']
+    disps = sorted(disps)
+
+    for fn in disps:
+        predix = fn[:-4]
+        calib_file = '{}/{}.txt'.format(args.calib_dir, predix)
+        calib = kitti_util.Calibration(calib_file)
+        disp_map = ssc.imread(args.disparity_dir + '/' + fn) / 256.
+        lidar = project_disp_to_depth(calib, disp_map, args.max_high)
+        # pad 1 in the indensity dimension
+        lidar = np.concatenate([lidar, np.ones((lidar.shape[0], 1))], 1)
+        lidar = lidar.astype(np.float32)
+        lidar.tofile('{}/{}.bin'.format(args.save_dir, predix))
+        print('Finish Depth {}'.format(predix))
